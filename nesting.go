2dab94a64dced01c45ad8d50777d4504b7da68a5
Bob Aman <bob@sporkmonger.com>
2019-10-29 23:15:42 -0700

    Unnest fields before shipping to Honeycomb

--- nesting.go
+++ nesting.go
@@ -1,6 +1,9 @@
 package ecsevent
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // Nest converts a map from dotted notation to a fully nested representation.
 func Nest(entry map[string]interface{}) map[string]interface{} {
@@ -23,6 +26,24 @@ func Nest(entry map[string]interface{}) map[string]interface{} {
 				// leaf node
 				currObj[segment] = value
 			}
+		}
+	}
+	return newEntry
+}
+
+// Unnest converts a map from a nested representation into flat dotted notation.
+func Unnest(entry map[string]interface{}) map[string]interface{} {
+	newEntry := make(map[string]interface{})
+	for key, value := range entry {
+		if mapValue, ok := value.(map[string]interface{}); ok {
+			// Nested value
+			unnestedMap := Unnest(mapValue)
+			for suffixKey, value := range unnestedMap {
+				newEntry[fmt.Sprintf("%s.%s", key, suffixKey)] = value
+			}
+		} else {
+			// Scalar value or list
+			newEntry[key] = value
 		}
 	}
 	return newEntry
